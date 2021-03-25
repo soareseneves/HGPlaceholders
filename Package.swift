0d66a1779e1a56a987c7d7fdba7157896d7ab2e9
Soares e Neves <soareseneves@terra.com.br>
2021-03-25 12:09:26 -0300

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "HGPlaceholders",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS("8.0")
+    ],
+    products: [
+        .library(name: "HGPlaceholders", targets: ["HGPlaceholders"])
+    ],
+    targets: [
+        .target(
+            name: "HGPlaceholders",
+            path: "HGPlaceholders",
+            exclude: [
+                "Info.plist",
+            ],
+            resources: [.process("Resources")]
+        )
+    ]
+)
